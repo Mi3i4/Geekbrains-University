5c1f74e526dceedde63fb9c78a4507a2de64b6ae
Dmitry <dmitrybosykh@Dmitrys-MacBook-Pro-2.local>
2018-06-26 16:31:58 +0300

    add git

---
+++ Yii/Lesson 2/Босых Дмитрий 2/htdocs/basic/models/Request.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "request".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $address
+ * @property string $email
+ * @property string $phone
+ * @property string $date_create
+ */
+class Request extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'request';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'address', 'email', 'phone', 'date_create'], 'required'],
+            [['date_create'], 'safe'],
+            [['name', 'address', 'email', 'phone'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'address' => 'Address',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'date_create' => 'Date Create',
+        ];
+    }
+}
